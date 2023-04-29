96a05be67d6d0de44c648f44255fabcee1575adf
AyimWilliam <odhisayim@gmail.com>
2023-04-29 11:04:55 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,20 @@
 import React, {useState} from "react";
 
 
+
 function Item({ name, category }) {
 
   const [isInCart, setIsInCart] = useState(false);
 
   const handleAddToCartButton = () =>{
     setIsInCart(!isInCart);
-
-    const liClass = isInCart ? "in-cart" : "";
   }
+    const liClass = isInCart ? "in-cart" : "";
   return (
     <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" onClick={handleCartButtonClick}>{isInCart ? "Remove From Cart": "Add to Cart"}</button>
+      <button className="add" onClick={handleAddToCartButton}>{isInCart ? "Remove From Cart": "Add to Cart"}</button>
     </li>
   );
 }
